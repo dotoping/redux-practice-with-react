b0b05cda6b5b711eae328f415644fe96e40994dc
Daniel <coding.dotping@gmail.com>
2021-07-26 21:34:34 +1000

    added comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk' // redux-thunk applied
 
 const middelware = [logger, thunk]
 
