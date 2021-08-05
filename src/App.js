d81eeb62fad2ffd3be2bffc38100d3436f86d104
Daniel <coding.dotping@gmail.com>
2021-08-05 22:37:03 +1000

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import Subscribers from './components/Subscribers';
 import store from './redux/store';
 import Display from './components/Display';
-// import Views from './components/Views';
 import Comments from './components/Comments';
 import Users from './components/Users';
 
