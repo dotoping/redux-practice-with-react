d2e3264a6253bf2a4f3f64bba90ec56a8c55e4f3
Daniel <coding.dotping@gmail.com>
2021-08-07 23:53:11 +1000

    comments added

--- src/redux/subscribers/actions.js
+++ src/redux/subscribers/actions.js
@@ -1,12 +1,13 @@
 import { ADD_SUBSCRIBER, REMOVE_SUBSCRIBER } from './types'
 
-
+// add subscriber
 export const addSubscriber = () => {
     return {
         type: ADD_SUBSCRIBER
     }
 }
 
+// remove subscriber
 export const removeSubscriber = () => {
     return {
         type: REMOVE_SUBSCRIBER
